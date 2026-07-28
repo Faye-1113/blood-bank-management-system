7d7ae018106fed7d890f89b12aaff1f402b422fc
Faye-1113 <chenzhif@kean.edu>
2026-07-28 23:08:39 +0800

    Add files via upload

---
+++ blood bank management system.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : BBMS
+ Source Server Type    : MySQL
+ Source Server Version : 50726 (5.7.26)
+ Source Host           : localhost:3306
+ Source Schema         :  blood bank management system
+
+ Target Server Type    : MySQL
+ Target Server Version : 50726 (5.7.26)
+ File Encoding         : 65001
+
+ Date: 24/05/2024 16:54:51
+*/
+
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module BloodBankManagementSystem {
+	requires java.sql;
+}
